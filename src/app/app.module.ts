0a9f161efa499c4d140356d65746079bca8eed14
WincentyKorobacz <wincenty.korobacz@g-group.dev>
2022-10-11 10:27:39 +0200

    initial config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterOutlet } from '@angular/router';
+import { NavModule } from './modules/nav/nav.module';
+import { APP_SERVICES } from './services';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterOutlet, NavModule],
+  providers: [APP_SERVICES],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
